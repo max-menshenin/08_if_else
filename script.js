abf652af50eea1b94de7ccf8ad084bb2b35ab7f3
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-01-17 12:38:20 +0300

    returned back

--- script.js
+++ script.js
@@ -12,12 +12,9 @@ orderNumberField.innerText = orderNumber;
 answerField.innerText = `Вы загадали число ${answerNumber }?`;
 
 document.getElementById('btnRetry').addEventListener('click', function () {
-    minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
-    maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
-    answerNumber = 0;
-    orderNumber = 1;
-    orderNumberField = ''
-    answerField = ''
+    minValue = 0;
+    maxValue = 100;
+    orderNumber = 0;
 })
 
 document.getElementById('btnOver').addEventListener('click', function () {
@@ -32,26 +29,6 @@ document.getElementById('btnOver').addEventListener('click', function () {
             gameRun = false;
         } else {
             minValue = answerNumber  + 1;
-            answerNumber  = Math.floor((minValue + maxValue) / 2);
-            orderNumber++;
-            orderNumberField.innerText = orderNumber;
-            answerField.innerText = `Вы загадали число ${answerNumber }?`;
-        }
-    }
-})
-
-document.getElementById('btnLess').addEventListener('click', function () {
-    if (gameRun){
-        if (minValue === maxValue){
-            const phraseRandom = Math.round( Math.random());
-            const answerPhrase = (phraseRandom === 1) ?
-                `Вы загадали неправильное число!\n\u{1F914}` :
-                `Я сдаюсь..\n\u{1F92F}`;
-
-            answerField.innerText = answerPhrase;
-            gameRun = false;
-        } else {
-            maxValue = answerNumber  + 1;
             answerNumber  = Math.floor((minValue + maxValue) / 2);
             orderNumber++;
             orderNumberField.innerText = orderNumber;
