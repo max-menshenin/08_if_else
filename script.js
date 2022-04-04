d6eb62cae3984c2250aab6d4366f5d39d970290a
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-04-04 20:54:37 +0300

    fixed less

--- script.js
+++ script.js
@@ -75,23 +75,26 @@ document.getElementById('btnOver').addEventListener('click', function () {
 })
 
 document.getElementById('btnLess').addEventListener('click', function () {
-    if (gameRun){
-        if (minValue === maxValue){
-            const phraseRandom = Math.round( Math.random());
-            const answerPhrase = (phraseRandom === 1) ?
-                `Вы загадали неправильное число!\n\u{1F914}` :
-                `Я сдаюсь..\n\u{1F92F}`;
-
-            answerField.innerText = answerPhrase;
-            gameRun = false;
-        } else {
-            maxValue = answerNumber - 1;
-            answerNumber  = Math.floor((minValue + maxValue) / 2);
-            orderNumber++;
-            orderNumberField.innerText = orderNumber;
-            answerField.innerText = `Вы загадали число ${answerNumber }?`;
-        }
-    }
+	if (gameRun) {
+		if (minValue === maxValue || minValue === answerNumber) {
+			const phraseRandom = Math.round(Math.random() * 2);
+			const answerPhrase = (phraseRandom === 1) ?
+				`Вы загадали неправильное число!\n\u{1F914}` :
+				`Я сдаюсь..\n\u{1F92F}`;
+			answerField.innerText = answerPhrase;
+			gameRun = false;
+		} else {
+			maxValue = answerNumber - 1;
+			answerNumber = Math.floor((minValue + maxValue) / 2);
+			orderNumber++;
+			orderNumberField.innerText = orderNumber;
+			const phraseRandom = Math.round(Math.random() * 2);
+			const answerPhrase = (phraseRandom === 0) ? `Вы загадали число ${answerNumber}?` :
+				(phraseRandom === 1) ? `Да это легко! Ты загадал ${answerNumber}!` :
+					`Наверное, это число ${answerNumber}.`;
+			answerField.innerText = answerPhrase;
+		}
+	}
 })
 
 
